178f26afcb819a7fb0f62e8b6e9084ddb4113bff
Grayhat.Fox <grayhat.fox@proton.me>
2022-12-09 13:35:15 +0300

    update(server): server structure

---
+++ server/app/app.js
@@ -0,0 +1,4 @@
+const Items = require('warframe-items');
+const warframes = new Items({category: ['Warframes']});
+
+console.log(warframes.length);
--- server/server.js
+++
@@ -1,4 +0,0 @@
-const Items = require('warframe-items');
-const warframes = new Items({category: ['Warframes']});
-
-console.log(warframes.length);
