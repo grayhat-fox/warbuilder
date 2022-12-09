8f211d340e4f78e9c50a653f3eb5232864539929
Grayhat.Fox <grayhat.fox@proton.me>
2022-12-09 10:52:26 +0300

    update(presentation): Initial Page UI

--- lib/project/presentation/pages/init_project_page.dart
+++ lib/project/presentation/pages/init_project_page.dart
@@ -5,9 +5,62 @@ class InitProjectPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const ScaffoldPage(
+    return ScaffoldPage(
+      padding: EdgeInsets.zero,
       content: SizedBox.expand(
-        child: Center(child: Text('test')),
+        child: Column(
+          children: [
+            const Expanded(
+              child: Center(
+                child: Text(
+                  'Warframe Builder',
+                  style: TextStyle(
+                    fontSize: 32.0,
+                    fontWeight: FontWeight.w600,
+                    letterSpacing: .5,
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Column(
+                children: [
+                  FilledButton(
+                    child: Container(
+                      padding: const EdgeInsets.all(8.0),
+                      width: MediaQuery.of(context).size.width / 3,
+                      child: const Text(
+                        'New Build',
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.w600,
+                          letterSpacing: .5,
+                        ),
+                      ),
+                    ),
+                    onPressed: () => null,
+                  ),
+                  const SizedBox(height: 16),
+                  FilledButton(
+                    child: Container(
+                      padding: const EdgeInsets.all(8.0),
+                      width: MediaQuery.of(context).size.width / 3,
+                      child: const Text(
+                        'Open Build',
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.w600,
+                          letterSpacing: .5,
+                        ),
+                      ),
+                    ),
+                    onPressed: () => null,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
